8364e7fb050bf92f85da5de3ca41b341f14ff585
aalai <DaniAkash@users.noreply.github.com>
2026-09-19 08:56:25 +0530

    fix: ordinal always returns the th suffix
    Closes #24

--- src/ordinal.ts
+++ src/ordinal.ts
@@ -1,2 +1,12 @@
 /** Returns the ordinal suffix form of a number, such as 1st or 22nd. */
-export const ordinal = (n: number): string => `${n}th`
+export const ordinal = (n: number): string => {
+  const finalTwoDigits = Math.abs(n) % 100
+  const finalDigit = Math.abs(n) % 10
+
+  if (finalTwoDigits >= 11 && finalTwoDigits <= 13) {
+    return `${n}th`
+  }
+
+  const suffix = finalDigit === 1 ? 'st' : finalDigit === 2 ? 'nd' : finalDigit === 3 ? 'rd' : 'th'
+  return `${n}${suffix}`
+}
